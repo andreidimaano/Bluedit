f6dd698e7fece947aad7481f847a159e5ca7e4a2
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-23 22:43:10 -0800

    add provider

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,18 +1,24 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import {Provider, createClient} from 'urql'
+
+const client = createClient({ url: 'https://localhost:4000/graphql' });
 
 import theme from '../theme'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </Provider>
+
   )
 }
 
