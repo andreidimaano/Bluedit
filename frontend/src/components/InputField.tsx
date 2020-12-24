34fb04c0a7126e2d52f0b733c56d2a8d6e490f4d
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-23 22:35:25 -0800

    add input field

---
+++ frontend/src/components/InputField.tsx
@@ -0,0 +1,24 @@
+import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react';
+import { Formik, Form, useField } from 'formik';
+import React from 'react'
+import { InputHTMLAttributes } from 'react';
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+    label: string;
+    name: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({label, size: _, ...props}) => {
+    const [field, {error}] = useField(props);
+    return (
+        <FormControl isInvalid={!!error}> {/* converts to boolean */}
+            <FormLabel htmlFor={field.name}>{label}</FormLabel>
+            <Input 
+                {...field} 
+                {...props} 
+                id={field.name}
+            />
+            {error? <FormErrorMessage>{error}</FormErrorMessage> : null}
+        </FormControl>
+    );
+}
