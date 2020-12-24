fbba2a8e81d527ce58a17a48233d1a881f82cf24
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-23 21:39:39 -0800

    finish backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,15 +7,15 @@ import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
-// import { UserResolver } from './resolvers/user';
+import { UserResolver } from './resolvers/user';
 import redis from 'redis'
 import session from 'express-session'
 import connectRedis from 'connect-redis';
-import { UserResolver } from './resolvers/user';
-// import cors from 'cors'
-// import { sendEmail } from './utils/sendEmail';
+import cors from 'cors'
+import { sendEmail } from './utils/sendEmail';
 
 const main = async () => {
+    sendEmail('bob@bob.com', 'hello there');
     const orm = await MikroORM.init(mikroOrmConfig);
     await orm.getMigrator().up();
 
@@ -23,6 +23,13 @@ const main = async () => {
 
     const RedisStore = connectRedis(session)
     let redisClient = redis.createClient()
+
+    app.use(
+        cors({
+            origin: 'http://localhost:3000',
+            credentials: true,
+        })
+    );
 
     app.use(
         session({
@@ -50,7 +57,10 @@ const main = async () => {
         context: ({req, res}) => ({ em: orm.em, req, res })
     });
 
-    apolloServer.applyMiddleware({app});
+    apolloServer.applyMiddleware({
+        app,
+        cors: false, 
+    });
 
     app.listen(4000, () => {
         console.log('server started on localhost:4000');
