4d6372dfb3987465ee7ff3de26da5bfca4f838fd
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-23 22:04:01 -0800

    initialize frontend

--- frontend/src/components/Wrapper.tsx
+++ frontend/src/components/Wrapper.tsx
@@ -1,22 +1,22 @@
-import { Box } from '@chakra-ui/react';
-import React from 'react'
+// import { Box } from '@chakra-ui/react';
+// import React from 'react'
 
-interface WrapperProps {
-    variant?: 'small' | 'regular'
-}
+// interface WrapperProps {
+//     variant?: 'small' | 'regular'
+// }
 
-export const Wrapper: React.FC<WrapperProps> = ({
-    children, //children of the wrapper
-    variant='regular'
-}) => {
-    return (
-        <Box 
-            mt={8} 
-            mx='auto' 
-            maxW={variant === 'regular' ? '800px' : '400px'}
-            w='100%'
-        >
-            {children}
-        </Box>
-    );
-}
+// export const Wrapper: React.FC<WrapperProps> = ({
+//     children, //children of the wrapper
+//     variant='regular'
+// }) => {
+//     return (
+//         <Box 
+//             mt={8} 
+//             mx='auto' 
+//             maxW={variant === 'regular' ? '800px' : '400px'}
+//             w='100%'
+//         >
+//             {children}
+//         </Box>
+//     );
+// }
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,5 @@
+const Index = () => (
+  <div>hello world</div>
+)
+
+export default Index
