a4e1d84e7e80f1421c43beae0565e72f83e9028a
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-24 14:37:08 -0800

    add navbar

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react'
+
+interface NavBarProps {
+
+}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+        return (
+            <Box bg="tomato">
+                pretty good
+            </Box>
+        );
+}
