1ab1f93e6ecf1840635b7862f7f9e7c3645588a0
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-23 22:29:59 -0800

    add wrapper

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Formik } from 'formik'
 import { Form } from 'formik';
 import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react';
+import { Wrapper } from '../components/Wrapper';
 
 interface registerProps {
 
@@ -9,26 +10,28 @@ interface registerProps {
 
 const Register: React.FC<registerProps> = ({}) => {
     return (
-        <Formik 
-            initialValues={{username: '', password: ''}}
-            onSubmit={(values) => {
-                console.log(values);
-            }}
-        >
-            {({values, handleChange}) => (
-                <Form>
-                    <FormControl>
-                        <FormLabel htmlFor="username">Username</FormLabel>
-                        <Input 
-                            value={values.username}
-                            onChange={handleChange} 
-                            id="username" 
-                            placeholder="username"
-                        />
-                    </FormControl>
-                </Form>                  
-            )}
-        </Formik>
+        <Wrapper>
+            <Formik 
+                initialValues={{username: '', password: ''}}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+            >
+                {({values, handleChange}) => (
+                    <Form>
+                        <FormControl>
+                            <FormLabel htmlFor="username">Username</FormLabel>
+                            <Input 
+                                value={values.username}
+                                onChange={handleChange} 
+                                id="username" 
+                                placeholder="username"
+                            />
+                        </FormControl>
+                    </Form>                  
+                )}
+            </Formik>            
+        </Wrapper>
     );
 };
 
