3814945e7e1cfc0740e0985ccde4fec0e54e70f9
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-24 15:50:38 -0800

    add posts to home page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,14 +2,22 @@ import { withUrqlClient } from "next-urql"
 import React from "react"
 import { NavBar } from "../components/NavBar"
 import { createUrqlClient } from "../utils/createUrqlClient"
+import { usePostsQuery } from "../generated/graphql"
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>
-      hello world
-    </div>
-  </>
-)
+const Index = () => {
+    const [{ data }] = usePostsQuery();
+    return (
+        <>
+            <NavBar />
+            <div>
+                hello world
+            </div>
+            <br/>
+            <div>
+                {!data ? null : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
+            </div>
+        </>
+    )
+}
 
 export default withUrqlClient(createUrqlClient)(Index);
