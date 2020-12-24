97ea66093dae9e01cbb1faced372805ebc327d15
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-24 14:28:40 -0800

    add register users

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -5,39 +5,27 @@ import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input } from '@c
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 import { useMutation } from 'urql';
+import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/router';
 
 interface registerProps {}
 
-const REGISTER_MUT = `
-mutation Register($username: String!, $email: String!, $password: String!) {
-    register(options: {username: $username, email: $email, password: $password}){
-      errors{
-        field
-        message
-      }
-      user {
-        username
-      }
-    }
-}
-`
-
 const Register: React.FC<registerProps> = ({}) => {
-    const [, register] = useMutation(REGISTER_MUT)
+    const router = useRouter();
+    const [, register] = useRegisterMutation();
     return (
         <Wrapper variant='small'>
             <Formik 
                 initialValues={{username: "", email: "", password: ""}}
-                onSubmit={(values) => {
-                    console.log(values);
-                    return register(values);
-                    // const response = await register(values);
-                    // if (response.data?.register.errors) {
-                    //     setErrors(toErrorMap(response.data.register.errors));
-                    // } else if (response.data?.register.user) {
-                    //     //register user successful
-                    //     router.push('/');
-                    // }
+                onSubmit={async (values, { setErrors }) => {
+                    const response = await register(values);
+                    if (response.data?.register.errors) {
+                        setErrors(toErrorMap(response.data.register.errors));
+                    } else if (response.data?.register.user) {
+                        //register user successful
+                        router.push('/');                        
+                    }
                 }}
             >
                 {(props) => (
