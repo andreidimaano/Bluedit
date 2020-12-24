69d48c5dd260a948e5838dd8c6e7be8874bb5949
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-23 20:27:30 -0800

    add resolver and apply middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,25 @@
 import { MikroORM } from '@mikro-orm/core'
 import { ___prod__ } from './constants';
-import { Post } from './entities/Post';
 import mikroOrmConfig from './mikro-orm.config';
-import microConfig from "./mikro-orm.config";
 import express from 'express'
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig);
     await orm.getMigrator().up();
 
     const app = express();
-    app.get('/', (_, res) => {
-        res.send("hello");
-    })
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        }),
+    });
+
+    apolloServer.applyMiddleware({app});
 
     app.listen(4000, () => {
         console.log('server started on localhost:4000');
