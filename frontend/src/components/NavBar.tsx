d6a3116904db0e0850caa72b0a046bab67ebfb48
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-24 14:46:36 -0800

    add nextlink

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Link } from '@chakra-ui/react';
 import React from 'react'
+import NextLink from "next/link";
 
 interface NavBarProps {
 
@@ -9,12 +10,16 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
         return (
             <Flex bg="tomato" p={4}>
                 <Box ml={"auto"}>
-                    <Link mr={2}>
-                        login
-                    </Link>
-                    <Link>
-                        register
-                    </Link>
+                    <NextLink href="/login">
+                        <Link mr={2}>
+                            login
+                        </Link>
+                    </NextLink>
+                    <NextLink href="/register">
+                        <Link>
+                            register
+                        </Link>
+                    </NextLink>
                 </Box>
             </Flex>
             
