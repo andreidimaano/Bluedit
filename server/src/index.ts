dc7a62d1f9df94c79c602670beac33f2143f0cbb
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-23 20:05:13 -0800

    add read operations

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,10 +5,13 @@ import mikroOrmConfig from './mikro-orm.config';
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig);
+    await orm.getMigrator().up();
 
     const post = orm.em.create(Post, {title: 'my first post'});
     await orm.em.persistAndFlush(post);
 
+    const posts = await orm.em.find(Post, {});
+    console.log(posts);
 }
 
 main().catch(err => {
