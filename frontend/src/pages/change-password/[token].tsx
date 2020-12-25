264317cd8da554385720e89d870f3c80e8b1844e
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-24 20:31:34 -0800

    change the change password page to form

--- frontend/src/pages/change-password/[token].tsx
+++ frontend/src/pages/change-password/[token].tsx
@@ -1,7 +1,56 @@
+import { Box, Button } from '@chakra-ui/react';
+import { Formik, Form } from 'formik';
 import { NextPage } from 'next';
+import router from 'next/dist/next-server/lib/router/router';
+import React from 'react';
+import { InputField } from '../../components/InputField';
+import { Wrapper } from '../../components/Wrapper';
+import { toErrorMap } from '../../utils/toErrorMap';
+import login from '../login';
 
 const ChangePassword: NextPage<{token: string}> = ({token}) => {
-    return <div>token is: {token}</div>;
+    return (
+    <Wrapper variant='small'>
+        <Formik 
+            initialValues={{usernameOrEmail: "", password: ""}}
+            onSubmit={async (values, { setErrors }) => {
+                const response = await login(values);
+                if (response.data?.login.errors) {
+                    setErrors(toErrorMap(response.data.login.errors));
+                } else if (response.data?.login.user) {
+                    //register user successful
+                    router.push('/');                        
+                }
+            }}
+        >
+            {(props) => (
+            <Form>
+                <InputField
+                    name="usernameOrEmail"
+                    placeholder="username or email"
+                    label="UsernameOrEmail"
+                />
+                <Box mt={4}>
+                    <InputField
+                        name="password"
+                        placeholder="password"
+                        label="Password"
+                        type="password"
+                    />
+                </Box>
+                <Button 
+                    mt={4} 
+                    type='submit' 
+                    isLoading={props.isSubmitting} 
+                    colorScheme='teal'
+                >
+                    login
+                </Button>
+            </Form>   
+            )}
+        </Formik>            
+    </Wrapper>
+    );
 }
 
 ChangePassword.getInitialProps =({query}) => {
