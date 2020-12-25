3524026ee48eb92d397638cd8b3f9c43184be5a9
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-24 21:20:06 -0800

    remove token

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -52,7 +52,8 @@ export class UserResolver {
             };
         }
 
-        const userId = await redis.get(FORGET_PASSWORD_PREFIX + token);
+        const key = FORGET_PASSWORD_PREFIX + token
+        const userId = await redis.get(key);
         if(!userId) {
             return {
                 errors: [
@@ -79,6 +80,7 @@ export class UserResolver {
 
         user.password = await argon2.hash(newPassword);
         await em.persistAndFlush(user);
+        await redis.del(key);
 
         req.session.userId = user.id;
 
