b09cfeda85382d599262c2afae3a14964ce47b92
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-24 16:17:18 -0800

    remove sleep function

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -2,13 +2,11 @@ import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql';
 import { Post } from '../entities/Post';
 import { MyContext } from '../types'
 
-const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
-
 @Resolver()
 export class PostResolver {
     @Query(() => [Post])
     async posts( @Ctx() {em}: MyContext): Promise<Post[]>  {
-        await sleep(3000)
+        // await sleep(3000)
         return em.find(Post, {});
     }
 
