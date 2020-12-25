342ba4f06b6e3a87dc57c4dd0fba1bf30069facf
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-24 16:50:25 -0800

    fix register forrmik on suubmit bug

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -21,7 +21,7 @@ const Register: React.FC<registerProps> = ({}) => {
             <Formik 
                 initialValues={{username: "", email: "", password: ""}}
                 onSubmit={async (values, { setErrors }) => {
-                    const response = await register(values);
+                    const response = await register({options: values});
                     if (response.data?.register.errors) {
                         setErrors(toErrorMap(response.data.register.errors));
                     } else if (response.data?.register.user) {
