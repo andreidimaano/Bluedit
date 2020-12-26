db313c14d837619421ec95f26be191a2079608d8
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-25 19:59:11 -0800

    update onSubmit

--- frontend/src/pages/create-post.tsx
+++ frontend/src/pages/create-post.tsx
@@ -1,25 +1,16 @@
-import { Box, Flex, Link, Button } from '@chakra-ui/react';
-import { Formik, Form } from 'formik';
-import router from 'next/dist/next-server/lib/router/router';
-import React from 'react'
+import { Box, Button } from '@chakra-ui/react';
+import { Form, Formik } from 'formik';
+import React from 'react';
 import { InputField } from '../components/InputField';
 import { Wrapper } from '../components/Wrapper';
-import { toErrorMap } from '../utils/toErrorMap';
-import login from './login';
 
 export const CreatePost: React.FC<{}> = ({}) => {
     return (
         <Wrapper variant="small">
             <Formik 
                 initialValues={{title: "", text: ""}}
-                onSubmit={async (values, { setErrors }) => {
-                    const response = await login(values);
-                    if (response.data?.login.errors) {
-                        setErrors(toErrorMap(response.data.login.errors));
-                    } else if (response.data?.login.user) {
-                        //register user successful
-                        router.push('/');                        
-                    }
+                onSubmit={async (values) => {
+                    console.log(values);
                 }}
             >
                 {(props) => (
