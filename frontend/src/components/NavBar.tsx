a2160807db66296a15aeea466e22f28575d7f596
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-25 21:02:29 -0800

    fix navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -50,7 +50,7 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
         );
     }
     return (
-        <Flex bg="tomato" p={4}>
+        <Flex zIndex={0} position="sticky" top={0} bg="tomato" p={4}>
             <Box ml={"auto"}>
                 {body}
             </Box>
