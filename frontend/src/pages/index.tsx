cbceb647d0ea8b835741b4421b3f8e2ea62094ba
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-25 21:16:26 -0800

    fix index

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,26 +1,28 @@
 import { withUrqlClient } from "next-urql"
 import React from "react"
-import { NavBar } from "../components/NavBar"
 import { createUrqlClient } from "../utils/createUrqlClient"
 import { usePostsQuery } from "../generated/graphql"
+import { Layout } from "../components/Layout"
+import NextLink from 'next/link'
+import { Link } from "@chakra-ui/react"
 
 const Index = () => {
     const [{ data }] = usePostsQuery();
     return (
-        <>
-            <NavBar />
-            <div>
-                hello world
-            </div>
+        <Layout>
+            <NextLink href="/create-post">
+                <Link>
+                    create post
+                </Link>
+            </NextLink>
             <br/>
-            <div>
-                {!data ? (
-                <div>loading...</div> 
-                ) : (
-                    data.posts.map((p) => <div key={p.id}>{p.title}</div>)
-                )}
-            </div>
-        </>
+            {!data ? (
+            <div>loading...</div> 
+            ) : (
+                data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+            )}
+
+        </Layout>
     )
 }
 
